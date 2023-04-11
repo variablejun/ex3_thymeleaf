8c64f2cd3340f43668c8acab91b064ef4ce56e79
variablejun <crescendo0217@gmail.com>
2023-04-11 15:08:17 +0900

    점심밥

--- src/main/java/com/example/ex3_thymeleaf/Controller/SampleController.java
+++ src/main/java/com/example/ex3_thymeleaf/Controller/SampleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,7 +23,7 @@ public class SampleController {
         log.info("ex1의 정보......");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(
                 i -> {
@@ -36,5 +37,24 @@ public class SampleController {
         ).collect(Collectors.toList());
         model.addAttribute("list", list);
     }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First...100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addAttribute("result", "Success");
+        redirectAttributes.addAttribute("dto", dto);
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3() 호출됨");
+    }
+
 
 }
